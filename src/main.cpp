efde926defb22b3403ac846971cc00b01d2a56b7
qualimock <qualimock@gmail.com>
2024-11-25 19:08:35 +0300

    use Directory in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "library/filediff.hpp"
 
 int main() {
+	Directory systemDir("/home/qualimock/dev/filediff/original");
+	Directory userDir("/home/qualimock/dev/filediff/scrambled");
+
+	systemDir.load();
+	userDir.load();
     return 0;
 }
