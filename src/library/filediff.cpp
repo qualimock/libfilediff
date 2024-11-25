8f8d882ce6711b47ca09cb245c0a1cffcc1f6ebe
qualimock <qualimock@gmail.com>
2024-11-25 19:11:21 +0300

    add directory comparison using set symmetric difference

--- src/library/filediff.cpp
+++ src/library/filediff.cpp
@@ -1,2 +1,12 @@
 #include "filediff.hpp"
 
+#include <iostream>
+#include <algorithm>
+
+FilesMap compareDirectories(const Directory& dirA, const Directory& dirB) {
+	FilesMap diff;
+	std::set_symmetric_difference(dirA.files().begin(), dirA.files().end(),
+								  dirB.files().begin(), dirB.files().end(),
+								  std::inserter(diff, diff.begin()));
+	return diff;
+}
--- src/library/filediff.hpp
+++ src/library/filediff.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
 #include "Directory.hpp"
+
+FilesMap compareDirectories(const Directory& dirA, const Directory& dirB);
