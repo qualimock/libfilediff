4997308623a8a83fd590e990ef932694775357a8
qualimock <qualimock@gmail.com>
2024-11-26 01:16:20 +0300

    add printing file chunk with custom borders from pivot

--- src/library/filediff.cpp
+++ src/library/filediff.cpp
@@ -45,3 +45,26 @@ Chunks compareFiles(const std::filesystem::path& file1, const std::filesystem::p
 
     return chunks;
 }
+
+std::string showFileChunk(const std::filesystem::path& file, unsigned left, unsigned right, std::pair<unsigned, unsigned> chunk) {
+	unsigned from = chunk.first - left;
+	unsigned to = chunk.second + right;
+
+	std::ifstream f(file, std::ios::binary | std::ios::ate);
+
+	if (!f.is_open()) {
+		std::cerr << "showFileChunk: Unable to open file" << std::endl;
+		return std::string();
+	}
+
+	f.seekg(from);
+
+	char byte;
+	std::string buf;
+
+	while (f.get(byte) && f.tellg() != to+1) {
+		buf += byte;
+	}
+
+	return buf;
+}
--- src/library/filediff.hpp
+++ src/library/filediff.hpp
@@ -8,3 +8,4 @@ typedef std::vector<std::pair<std::streamoff, std::streamoff>> Chunks;
 
 FilesMap compareDirectories(const Directory& dirA, const Directory& dirB);
 Chunks compareFiles(const std::filesystem::path& file1, const std::filesystem::path& file2);
+std::string showFileChunk(const std::filesystem::path& file, unsigned left, unsigned right, std::pair<unsigned, unsigned> chunk);
