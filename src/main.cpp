59765b49cafaed70baf7550ddbf94f30ef1d4882
qualimock <qualimock@gmail.com>
2024-11-25 20:13:52 +0300

    check library work

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 #include "library/filediff.hpp"
 
 int main() {
-	Directory systemDir("/home/qualimock/dev/filediff/original");
+	Directory systemDir;
 	Directory userDir("/home/qualimock/dev/filediff/scrambled");
 
 	systemDir.load();
 	userDir.load();
+
+	auto diff = compareDirectories(systemDir, userDir);
+
+	std::cout << "Tampered files:" << std::endl;
+	for (auto file : diff) {
+		std::cout << file.first << std::endl;
+	}
     return 0;
 }
