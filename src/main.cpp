4a48f267ea48a2e75d411057564a1bbe9ca5493a
qualimock <qualimock@gmail.com>
2024-11-26 01:16:40 +0300

    test file comparison

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,29 @@ int main() {
 		auto systemFile = systemDir.files().find(file.first);
 		auto userFile = userDir.files().find(file.first);
 
-		std::cout << "System file: " << systemDir.path() << systemFile->first.generic_string() << "    XXH64: " << systemFile->second << std::endl;
-		std::cout << "User file: " << userDir.path() << userFile->first.generic_string() << "      XXH64: " << userFile->second << std::endl;
+		std::cout << "System file: " << systemDir.path() << systemFile->first.generic_string()
+				  << std::endl << "XXH64: " << systemFile->second << std::endl;
 		std::cout << std::endl;
+
+		std::cout << "User file: " << userDir.path() << userFile->first.generic_string()
+				  << std::endl << "XXH64: " << userFile->second << std::endl;
+		std::cout << std::endl;
+
+		auto file1 = std::filesystem::path(systemDir.path() + systemFile->first.generic_string());
+		auto file2 = std::filesystem::path(userDir.path() + userFile->first.generic_string());
+		auto chunks = compareFiles(file1, file2);
+
+		for (auto chunk : chunks) {
+			std::cout << systemDir.path() + systemFile->first.generic_string() << ": " << std::endl;
+			std::cout << showFileChunk(systemDir.path() + systemFile->first.generic_string(), 50, 50, chunk) << std::endl;
+			std::cout << std::endl;
+
+            std::cout << userDir.path() + userFile->first.generic_string() << ": " << std::endl;
+			std::cout << showFileChunk(userDir.path() + userFile->first.generic_string(), 50, 50, chunk) << std::endl;
+			std::cout << std::endl;
+		}
+
+		std::cout << "----------------------------" << std::endl;
 	}
 
 	return 0;
