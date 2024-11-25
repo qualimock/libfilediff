9aa190052f5347314ee3c44ff0ac78bdf7cae885
qualimock <qualimock@gmail.com>
2024-11-26 01:12:49 +0300

    add file byte-to-byte comparison

--- src/library/filediff.cpp
+++ src/library/filediff.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <algorithm>
+#include <fstream>
 
 FilesMap compareDirectories(const Directory& dirA, const Directory& dirB) {
 	FilesMap diff;
@@ -9,4 +10,38 @@ FilesMap compareDirectories(const Directory& dirA, const Directory& dirB) {
 								  dirB.files().begin(), dirB.files().end(),
 								  std::inserter(diff, diff.begin()));
 	return diff;
+}
+
+Chunks compareFiles(const std::filesystem::path& file1, const std::filesystem::path& file2) {
+    std::ifstream f1(file1, std::ios::binary | std::ios::ate);
+    std::ifstream f2(file2, std::ios::binary | std::ios::ate);
+
+    if (!f1.is_open() || !f2.is_open()) {
+        std::cerr << "compareFiles: Unable to open one or both files!" << std::endl;
+		return Chunks();
+    }
+
+	f1.seekg(0);
+    f2.seekg(0);
+
+	Chunks chunks;
+
+    char byte1, byte2;
+	std::streamoff begin, end;
+	bool foundCorruption = false;
+
+    while (f1.get(byte1) && f2.get(byte2)) {
+        if (byte1 != byte2 && !foundCorruption) {
+			begin = f1.tellg();
+			foundCorruption = true;
+        }
+
+		if (byte1 == byte2 && foundCorruption) {
+			end = f1.tellg();
+			chunks.push_back(std::make_pair(begin, end));
+			foundCorruption = false;
+		}
+    }
+
+    return chunks;
 }
--- src/library/filediff.hpp
+++ src/library/filediff.hpp
@@ -2,4 +2,9 @@
 
 #include "Directory.hpp"
 
+#include <vector>
+
+typedef std::vector<std::pair<std::streamoff, std::streamoff>> Chunks;
+
 FilesMap compareDirectories(const Directory& dirA, const Directory& dirB);
+Chunks compareFiles(const std::filesystem::path& file1, const std::filesystem::path& file2);
