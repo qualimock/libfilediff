55b9324405b7db41bca513dd26adf37fde3572f0
qualimock <qualimock@gmail.com>
2024-11-26 21:12:37 +0300

    join extern "C" into single block

--- src/library/filediff.cpp
+++ src/library/filediff.cpp
@@ -4,9 +4,16 @@
 #include <fstream>
 #include <cstring>
 
-extern "C" Borders createBorders(long left, long right) {
-    Borders b{left, right};
-    return b;
+extern "C" {
+    Borders createBorders(long left, long right) {
+        Borders b{left, right};
+        return b;
+    }
+
+    const char* cgetFileChunk(const char* file, Borders* aroundChunk, Borders* chunkBorders) {
+        const char* chunk = strdup(getFileChunk(std::filesystem::path(file), aroundChunk, chunkBorders).c_str());
+        return chunk;
+    }
 }
 
 std::string getFileChunk(const std::filesystem::path& file, Borders* aroundChunk, Borders* chunk) {
@@ -30,11 +37,6 @@ std::string getFileChunk(const std::filesystem::path& file, Borders* aroundChunk
     }
 
     return buf;
-}
-
-extern "C" const char* cgetFileChunk(const char* file, Borders* aroundChunk, Borders* chunkBorders) {
-    const char* chunk = strdup(getFileChunk(std::filesystem::path(file), aroundChunk, chunkBorders).c_str());
-    return chunk;
 }
 
 std::vector<Borders> compareFiles(const std::filesystem::path& file1, const std::filesystem::path& file2) {
--- src/library/filediff.h
+++ src/library/filediff.h
@@ -2,15 +2,18 @@
 
 #include "filediff_directory.h"
 
-extern "C"
-typedef struct {
-    long left;
-    long right;
-} Borders;
+extern "C" {
+    typedef struct {
+        long left;
+        long right;
+    } Borders;
 
-extern "C" Borders createBorders(long left, long right);
+    Borders createBorders(long left, long right);
 
-extern "C" const char* cgetFileChunk(const char* file, Borders* aroundChunk, Borders* chunkBorders);
+    const char *cgetFileChunk(const char *file,
+                              Borders *aroundChunk,
+                              Borders *chunkBorders);
+}
 
 std::string getFileChunk(const std::filesystem::path& file, Borders* aroundChunk, Borders* chunkBorders);
 std::vector<Borders> compareFiles(const std::filesystem::path& file1, const std::filesystem::path& file2);
