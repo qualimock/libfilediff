056b96c41f7f526fcc531b9135115914913574a6
qualimock <qualimock@gmail.com>
2024-11-27 12:04:57 +0300

    move C wrapper to another branch

--- src/library/filediff.cpp
+++ src/library/filediff.cpp
@@ -2,56 +2,10 @@
 
 #include <iostream>
 #include <fstream>
-#include <cstring>
 
-extern "C" {
-    Borders createBorders(long left, long right) {
-        Borders b{left, right};
-        return b;
-    }
-
-    const char* cgetFileChunk(const char* file, Borders* aroundChunk, Borders* chunkBorders) {
-        const char* chunk = strdup(getFileChunk(std::filesystem::path(file), aroundChunk, chunkBorders).c_str());
-        return chunk;
-    }
-
-    void ccompareDirectories(char**& output_diff, const char* dir1, const char* dir2) {
-        Directory d1(dir1);
-        Directory d2(dir2);
-
-        std::cout << "Loading directory " << dir1 << std::endl;
-        if (!d1.load()) {
-            std::cerr << "Cannot load " << dir1 << std::endl;
-            return;
-        }
-
-        std::cout << "Loading directory " << dir2 << std::endl;
-        if (!d2.load()) {
-            std::cerr << "Cannot load " << dir2 << std::endl;
-            return;
-        }
-
-        auto diff = d1.compare(d2);
-
-        output_diff = (char**)malloc((diff.size()+1) * sizeof(char*));
-
-        int count = 0;
-        for (auto file : diff) {
-            output_diff[count] = strdup(file.first.c_str());
-            count++;
-        }
-
-        output_diff[count] = NULL;
-    }
-
-    void ccompareFiles(Borders*& output_borders, const char* file1, const char* file2) {
-        std::vector<Borders> borders = compareFiles(file1, file2);
-        output_borders = (Borders*)malloc(sizeof(borders) * sizeof(Borders));
-
-        for (size_t i = 0; i < borders.size(); ++i) {
-            output_borders[i] = borders[i];
-        }
-    }
+Borders createBorders(long left, long right) {
+    Borders b{left, right};
+    return b;
 }
 
 std::string getFileChunk(const std::filesystem::path& file, Borders* aroundChunk, Borders* chunk) {
--- src/library/filediff.h
+++ src/library/filediff.h
@@ -2,26 +2,12 @@
 
 #include "filediff_directory.h"
 
-extern "C" {
-    typedef struct {
-        long left;
-        long right;
-    } Borders;
+typedef struct {
+    long left;
+    long right;
+} Borders;
 
-    Borders createBorders(long left, long right);
-
-    const char *cgetFileChunk(const char* file,
-                              Borders* aroundChunk,
-                              Borders* chunkBorders);
-
-    // remember to free output_diff after usage
-    void ccompareDirectories(char**& output_diff,
-                             const char* dir1,
-                             const char* dir2);
-
-    // remember to free output_borders after usage
-    void ccompareFiles(Borders*& output_borders, const char* file1, const char* file2);
-}
+Borders createBorders(long left, long right);
 
 std::string getFileChunk(const std::filesystem::path& file,
                          Borders* aroundChunk,
