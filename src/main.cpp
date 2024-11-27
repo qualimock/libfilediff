78bce2a291ac1a05d30d13542cae3e387c6f159e
qualimock <qualimock@gmail.com>
2024-11-27 12:01:33 +0300

    test wrappers

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,48 @@
 #include <iostream>
 #include "library/filediff.h"
 
+#include <cstring>
+
 int main() {
-    Directory systemDir;
-    Directory userDir("/home/qualimock/dev/libfilediff/scrambled/");
+    const char* dir1 = "/usr/bin/";
+    const char* dir2 = "/home/qualimock/dev/libfilediff/scrambled/";
 
-    if (!systemDir.load()) {
-        std::cerr << "Cannot load system dir" << std::endl;
-        return -1;
-    }
+    char** diff;
+    ccompareDirectories(diff, dir1, dir2);
 
-    if (!userDir.load()) {
-        std::cerr << "Cannot load user dir" << std::endl;
-        return -1;
-    }
+    for (int i = 0; diff[i] != NULL; ++i) {
+        char* file1 = (char*)malloc(strlen(dir1) + strlen(diff[i]) + 1);
+        strcpy(file1, dir1);
+        strcat(file1, diff[i]);
 
-    auto diff = systemDir.compare(userDir);
+        char* file2 = (char*)malloc(strlen(dir2) + strlen(diff[i]) + 1);
+        strcpy(file2, dir2);
+        strcat(file2, diff[i]);
 
-    std::cout << std::endl << "Tampered files:" << std::endl;
+        Borders* borders;
+        ccompareFiles(borders, file1, file2);
 
-    for (auto file : diff) {
-        auto systemFile = systemDir.files().find(file.first);
-        auto userFile = userDir.files().find(file.first);
+        Borders around = createBorders(20, 20);
 
-        auto chunks = compareFiles(systemDir.path() + systemFile->first.generic_string(),
-                                   userDir.path() + userFile->first.generic_string());
+        std::cout << diff[i] << ":" << std::endl;
 
-        std::cout << "System file: " << systemDir.path() << systemFile->first.generic_string()
-                  << std::endl << "XXH64: " << systemFile->second << std::endl;
-        std::cout << std::endl;
+        for (int j = 0; j != (sizeof(*borders)/sizeof(borders[0])); ++j) {
+            std::cout << file1 << std::endl;
+            std::cout << borders[j].left << ":" << borders[j].right << std::endl;
+            std::cout << cgetFileChunk(file1, &around, &borders[j]) << std::endl;
 
-        std::cout << "User file: " << userDir.path() << userFile->first.generic_string()
-                  << std::endl << "XXH64: " << userFile->second << std::endl;
-        std::cout << std::endl;
-
-        for (auto chunk : chunks) {
-            std::cout << systemDir.path() + systemFile->first.generic_string() << ": " << std::endl;
-            Borders aroundChunk = createBorders(50, 50);
-            const char* chunkStr = cgetFileChunk((systemDir.path() + systemFile->first.generic_string()).c_str(), &aroundChunk, &chunk);
-            std::cout << chunkStr << std::endl;
             std::cout << std::endl;
 
-            std::cout << userDir.path() + userFile->first.generic_string() << ": " << std::endl;
-            chunkStr = cgetFileChunk((userDir.path() + userFile->first.generic_string()).c_str(), &aroundChunk, &chunk);
-            std::cout << chunkStr << std::endl;
-            std::cout << std::endl;
+            std::cout << file2 << std::endl;
+            std::cout << borders[j].left << ":" << borders[j].right << std::endl;
+            std::cout << cgetFileChunk(file2, &around, &borders[j]) << std::endl;
         }
 
-        std::cout << "----------------------------" << std::endl;
+        std::cout << "-----------------" << std::endl;
+
+        free(borders);
     }
 
+    free(diff);
     return 0;
 }
