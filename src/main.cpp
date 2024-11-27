0e505818718d6cf7a964419a36f960c35adc6e10
qualimock <qualimock@gmail.com>
2024-11-27 14:36:53 +0300

    test library

--- src/main.cpp
+++ src/main.cpp
@@ -4,45 +4,35 @@
 #include <cstring>
 
 int main() {
-    const char* dir1 = "/usr/bin/";
-    const char* dir2 = "/home/qualimock/dev/libfilediff/scrambled/";
+    Directory dir1;
+    Directory dir2("/home/qualimock/dev/libfilediff/scrambled/");
 
-    char** diff;
-    ccompareDirectories(diff, dir1, dir2);
+    dir1.load();
+    dir2.load();
 
-    for (int i = 0; diff[i] != NULL; ++i) {
-        char* file1 = (char*)malloc(strlen(dir1) + strlen(diff[i]) + 1);
-        strcpy(file1, dir1);
-        strcat(file1, diff[i]);
+    auto diff = dir1.compare(dir2);
 
-        char* file2 = (char*)malloc(strlen(dir2) + strlen(diff[i]) + 1);
-        strcpy(file2, dir2);
-        strcat(file2, diff[i]);
+    Borders aroundChunk = createBorders(20, 20);
 
-        Borders* borders;
-        ccompareFiles(borders, file1, file2);
+    for (auto file : diff) {
+        fs::path file1 = dir1.path().generic_string() + file.first.generic_string();
+        fs::path file2 = dir2.path().generic_string() + file.first.generic_string();
 
-        Borders around = createBorders(20, 20);
+        std::vector<Borders> v_borders = compareFiles(file1, file2);
 
-        std::cout << diff[i] << ":" << std::endl;
-
-        for (int j = 0; j != (sizeof(*borders)/sizeof(borders[0])); ++j) {
-            std::cout << file1 << std::endl;
-            std::cout << borders[j].left << ":" << borders[j].right << std::endl;
-            std::cout << cgetFileChunk(file1, &around, &borders[j]) << std::endl;
+        std::cout << "File " <<  file.first.generic_string() << std::endl;
+        for (Borders chunkBorders: v_borders) {
+            std::cout << file1.generic_string() << ":" << std::endl;
+            std::cout << getFileChunk(file1, &aroundChunk, &chunkBorders) << std::endl;
 
             std::cout << std::endl;
 
-            std::cout << file2 << std::endl;
-            std::cout << borders[j].left << ":" << borders[j].right << std::endl;
-            std::cout << cgetFileChunk(file2, &around, &borders[j]) << std::endl;
+            std::cout << file2.generic_string() << ":" << std::endl;
+            std::cout << getFileChunk(file2, &aroundChunk, &chunkBorders) << std::endl;
         }
 
-        std::cout << "-----------------" << std::endl;
-
-        free(borders);
+        std::cout << "--------------------------------" << std::endl;
     }
 
-    free(diff);
     return 0;
 }
